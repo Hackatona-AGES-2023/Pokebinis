b452f33c42d855b7e2ba561543fd3a7c1c667da8
PedroCarlucci <pkcarlucci@gmail.com>
2023-05-27 05:09:39 -0300

    partial commit prompt page

--- frontend/src/pages/Prompt/index.tsx
+++ frontend/src/pages/Prompt/index.tsx
@@ -6,14 +6,14 @@ const Prompt: React.FC = () => {
 	return <div className='prompt-container'>
 		<header className="header">
 			<div className="header-content">
-				<h1>teste</h1>
+				
 			</div>
 		</header>
 		<div className='content'>
 			<h1 className="title">Qual sua carreira</h1>
 			<Input
-				label="Email de Acesso"
-				required={true}
+				label="Profissão"
+				required={false}
 				className="input"
 			/>
 		</div>
