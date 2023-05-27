044b31579825bb83d6caba41cc8c7883a16e8acf
Arthur Kunzler <arthur.gabriel.k@gmail.com>
2023-05-27 06:36:16 -0300

    ajustando encapsulamentos do openai

--- backend/app/apis/core/prompt/service.py
+++ backend/app/apis/core/prompt/service.py
@@ -5,7 +5,9 @@ class PromptService:
     
 	@staticmethod
 	def create(carrer: str):
-		res = openai.get_topics(carrer)
-		return json.loads(res) 
+		prompt = "Me de 5 topicos que eu precio ter conhecimento avançado para desenvolver minha carreira na area: {}. De os topicos em formato JSON, com uma chave 'topics' contendo uma lista com os 5 topicos".format(carrer)
+		res = openai.prompt(prompt, max_tokens=100)
+		res = res.get("choices")[0].get('text').strip()
+		return json.loads(res)
 
 	
--- backend/app/base/openai/openai.py
+++ backend/app/base/openai/openai.py
@@ -14,12 +14,13 @@ class OpenAI:
 		self._api_key = app.config.get('OPEN_AI_API_KEY')
 		openai.api_key = self._api_key
 
-	def get_topics(self, prompt: str):
+	def prompt(self, prompt: str, max_tokens : int):
 		response = openai.Completion.create(
 			model="text-davinci-003",
-			prompt="Me de 5 topicos que eu precio ter conhecimento avançado para desenvolver minha carreira na area: {}. De os topicos em formato JSON, com uma chave 'topics' contendo uma lista com os 5 topicos".format(prompt),
+			prompt=prompt,
 			temperature=0,
-			max_tokens=100
+			max_tokens=max_tokens
 		)
-		return response.get("choices")[0].get('text').strip()
+		return response
+
 		
