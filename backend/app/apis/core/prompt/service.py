05ff3db0334c15261c5fe46a055d3863d6581cbc
Arthur Kunzler <arthur.gabriel.k@gmail.com>
2023-05-27 14:58:03 -0300

    ajuste de typo do prompt

--- backend/app/apis/core/prompt/service.py
+++ backend/app/apis/core/prompt/service.py
@@ -1,3 +1,4 @@
+# from app.apis.core.prompt.repository import PromptRepository
 from app.ext.openai import openai
 import json
 
@@ -5,9 +6,10 @@ class PromptService:
     
 	@staticmethod
 	def get_topics_from_carrer(carrer: str):
-		prompt = "Me de 5 topicos que eu precio ter conhecimento avançado para desenvolver minha carreira na area: {}. De os topicos em formato JSON, com uma chave 'topics' contendo uma lista com os 5 topicos".format(carrer)
+		prompt = "Me de 5 topicos que eu preciso ter conhecimento avançado para desenvolver minha carreira na area: {}. De os topicos em formato JSON, com uma chave 'topics' contendo uma lista com os 5 topicos".format(carrer)
 		res = openai.prompt(prompt, max_tokens=100)
 		res = res.get("choices")[0].get('text').strip()
+		# PromptRepository.create_carrer(carrer)
 		return json.loads(res)
 
 	@staticmethod
